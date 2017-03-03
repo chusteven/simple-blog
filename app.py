5e390dac8821110d4a885f0c20a15a992109ce13
Steven Chu <stevenc1@gmail.com>
2017-03-02 19:31:30 -0800

    first commit of personal blog

---
+++ app.py
@@ -0,0 +1,369 @@
+#!blog/bin/python
+
+
+
+
+
+#
+# Imports
+#
+
+import datetime
+import functools
+import os
+import re
+import urllib
+
+# todo: research abort, flash, request, Response
+# todo: research how url_for() method works
+from flask import (Flask, abort, flash, Markup, redirect, render_template, 
+	request, Response, session, url_for)
+
+# todo: research
+from markdown import markdown
+from markdown.extensions.codehilite import CodeHiliteExtension
+from markdown.extensions.extra import ExtraExtension
+
+# todo: research
+from micawber import bootstrap_basic, parse_html
+from micawber.cache import Cache as OEmbedCache
+
+# todo: research
+from peewee import *
+
+# todo: research
+from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
+from playhouse.sqlite_ext import *
+
+
+
+
+
+#
+# Application constants
+#
+
+ADMIN_PASSWORD = os.environ.get("FLASK_BLOG_ADMIN_PASSWORD", "password")
+
+APP_DIR = os.path.dirname(os.path.realpath(__file__))
+DATABASE_PATH = os.path.join(APP_DIR, "blog.db")
+DATABASE = "sqliteext:///{}".format(DATABASE_PATH)
+
+DEBUG = False
+
+# todo: externalize this key to an more secure location (or use one-way hashing)
+# todo: research below statement's meaning
+# used by Flask to encrypt session cookie
+SECRET_KEY = "shhh, secret!"  
+SITE_WIDTH = 800
+
+
+
+
+
+#
+# Initializing application and all necessary components (configurations, etc.)
+#
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# todo: research
+flask_db = FlaskDB(app)
+database = flask_db.database
+
+# todo: research
+oembed_providers = bootstrap_basic(OEmbedCache())
+
+
+
+
+
+#
+# Entities
+#
+
+# todo: research how subclassing works in Python
+class Entry(flask_db.Model):
+	title = CharField() # maybe this object comes from peewee...
+	slug = CharField(unique = True) # note: constraints can apply
+	content = TextField()
+	published = BooleanField(index = True) # note: how to index
+	timestamp = DateTimeField(default = datetime.datetime.now, index = True) # note: how to provide defaults
+
+	@property
+	def html_content(self):
+		# todo: research what all these things do
+		hilite = CodeHiliteExtension(linenums = False, css_class = "highlight")
+		extras = ExtraExtension()
+		markdown_content = markdown(self.content, extensions = [hilite, extras])
+		oembed_content = parse_html(
+			markdown_content, 
+			oembed_providers, 
+			urlize_all = True, 
+			maxwidth = app.config["SITE_WIDTH"])
+		return Markup(oembed_content)
+
+	def save(self, *args, **kwargs):
+		# ...
+		if not self.slug:
+			self.slug = re.sub('[^\w]+', '-', self.title.lower())
+
+		# todo: research what this is doing under the hood
+		# 	- what is happening with the fields?
+		# 	- what is returned?
+		ret = super(Entry, self).save(*args, **kwargs)
+
+		# todo: research
+		self.update_search_index()
+
+		# todo: why returning?
+		return ret
+
+	# todo: research why need to pass self here
+	def update_search_index(self):
+		try:
+			fts_entry = FTSEntry.get(FTSEntry.entry_id == self.id)
+		except FTSEntry.DoesNotExist:
+			print("This is a new record!!!")
+			fts_entry = FTSEntry(entry_id = self.id)
+			force_insert = True
+		else:
+			print("We should be persisting this record!!!")
+			force_insert = False
+		# one long string is title plus the content
+		fts_entry.content = "\n".join((self.title, self.content))
+
+		# todo: research if force_insert is something like "upsert" or not
+		fts_entry.save(force_insert = force_insert)
+
+	@classmethod
+	def public(cls):
+		return Entry.select().where(Entry.published == True)
+
+	@classmethod
+	def search(cls, query):
+		words = [word.strip() for word in query.split() if word.strip()]
+		if not words:
+			# return empty entity???
+			return Entry.select().where(Entry.id == 0)
+		else:
+			search = " ".join(words)
+
+		# todo: research how this kind of in-code query works
+		# http://charlesleifer.com/blog/using-sqlite-full-text-search-with-python/
+		return (FTSEntry
+			.select(
+				FTSEntry, 
+				Entry, 
+				FTSEntry.rank().alias("score"))
+			.join(Entry, on = (FTSEntry.entry_id == Entry.id).alias("entry"))
+			.where(
+				(Entry.published == True) & 
+				(FTSEntry.match(search)))
+			.order_by(SQL("score").desc()))
+
+	@classmethod
+	def drafts(cls):
+		return Entry.select().where(Entry.published == False)
+
+class FTSEntry(FTSModel):
+	entry_id = IntegerField()
+	content = TextField()
+
+	# todo: research what use this is
+	class Meta:
+		database = database
+
+#
+# Finish this entity
+#
+
+class Comment(flask_db.Model):
+
+	author = CharField()
+	content = TextField()
+	timestamp = DateTimeField(default = datetime.datetime.now, index = True)
+
+	def save(self, *args, **kwargs):
+		return super(Comment, self).save(*args, **kwargs)
+
+
+
+
+
+#
+# Adding some handlers
+#
+
+def login_required(fn):
+	@functools.wraps(fn)
+	def inner(*args, **kwargs):
+		if session.get("logged_in"):
+			return fn(*args, **kwargs)
+		return redirect(url_for("login", next = request.path))
+	return inner
+
+@app.route("/login/", methods = ["GET", "POST"])
+def login():
+	# takes query parameters and takes user to the page they want after logging in
+	next_url = request.args.get("next") or request.form.get("next")
+
+	# if POST (ie. logging in), then send them to appropriate location (generally, the index page)
+	if request.method == "POST" and request.form.get("password"):
+		password = request.form.get("password")
+		if password == app.config["ADMIN_PASSWORD"]:
+			session["logged_in"] = True
+
+			# todo: research this as well as statement below
+			# use cookie to store session
+			session.permanent = True  
+			flash("You are now logged in.", "success")
+			return redirect(next_url or url_for("index"))
+		else:
+			flash("Incorrect password.", "danger")
+	# if GET (ie. want to go to login), then send to login page
+	return render_template("login.html", next_url = next_url)
+
+@app.route("/logout/", methods = ["GET", "POST"])
+def logout():
+	if request.method == "POST":
+		# cleaning all session of its attributes
+		session.clear()
+		return redirect(url_for("login"))
+	return render_template("logout.html")
+
+@app.route("/")
+def index():
+	search_query = request.args.get("q")
+	# interesting way to render the home page!
+	if search_query:
+		query = Entry.search(search_query)
+	else:
+		query = Entry.public().order_by(Entry.timestamp.desc())
+
+	# because, either way, shit gets rendered!
+	# todo: research why i had to add check_bounds = false
+	return object_list("index.html", query, search = search_query, check_bounds = False)
+
+@app.route("/drafts/")
+@login_required
+def drafts():
+	query = Entry.drafts().order_by(Entry.timestamp.desc())
+	# todo: research why check_bounds is false
+	# http://docs.peewee-orm.com/en/latest/peewee/playhouse.html
+	return object_list("index.html", query, check_bounds  = False)
+
+
+### THIS IS A UTILITY METHOD
+def _create_or_edit(entry, template):
+	# if submitting data
+	if request.method == "POST":
+		# extract data (kind of like "or-equals" in Ruby)
+		entry.title = request.form.get("title") or ""
+		entry.content = request.form.get("content") or ""
+		entry.published = request.form.get("published") or False
+
+
+
+		# todo: complete this and below
+		# fentry = FTSEntry.select().where()
+
+
+		if not (entry.title and entry.content):
+			flash("Title and Content are required.", "danger")
+		else:
+			# wrap the call to save in a transaction so we can roll it back
+			# cleanly in the event of an integrity error
+			try:
+				with database.atomic():
+					# todo: make sure the appropriate entry in the Metatable is updated
+					entry.save()
+			except IntegrityError:
+				flash("Error: this title is already in use.", "danger")
+			else:
+				flash("Entry saved successfully.", "success")
+				# appropriately redirect, dependent on what type of POST we made
+				if entry.published:
+					return redirect(url_for("detail", slug = entry.slug))
+				else:
+					return redirect(url_for("edit", slug = entry.slug))
+
+	# if simply navigating to this page, then return appropriate page
+	return render_template(template, entry = entry)
+
+
+@app.route("/create/", methods = ["GET", "POST"])
+@login_required
+def create():
+	# an empty Entry (not yet persisted)
+	return _create_or_edit(Entry(title = "", content = ""), "create.html")
+
+@app.route("/<slug>/edit/", methods = ["GET", "POST"])
+@login_required
+def edit(slug):
+	# first get the correct entry, then redirect
+	entry = get_object_or_404(Entry, Entry.slug == slug)
+	return _create_or_edit(entry, "edit.html")
+
+@app.route("/<slug>/")
+def detail(slug):
+	# makes sense to have these two things for public and private viewing
+	# todo: think about whether there would be any use of "private" entries other than for drafts
+	if session.get("logged_in"):
+		query = Entry.select()
+	else:
+		query = Entry.public()
+	# todo: research how this method works
+	entry = get_object_or_404(query, Entry.slug == slug)
+	return render_template("detail.html", entry = entry)
+
+
+
+
+
+#
+# Application intiailization code
+#
+
+@app.template_filter("clean_querystring")
+def clean_querystring(request_args, *keys_to_remove, **new_values):
+	# assume some data will be passed along in request argument object
+	# todo: research more about the request object
+	querystring = dict((key, value) for key, value in request_args.items())
+
+	# simple removes these entries from the dictionary
+	for key in keys_to_remove:
+		querystring.pop(key, None)
+
+	# todo: research what update() method does for a dict object
+	querystring.update(new_values)
+	return urllib.urlencode(querystring) # HTTP-compatible rendering of dictionary object?
+
+@app.errorhandler(404)
+def not_found(exc):
+	# seems like it returns fully rendered HTML plus a response code
+	return Response("<h3>Not found</h3>"), 404
+
+def main():
+	# this creates tables if non existent already
+	# todo: research what the safe argument is good for
+	database.create_tables([Entry, FTSEntry], safe = True)
+	app.run(debug = True)
+
+if __name__ == "__main__":
+	main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
